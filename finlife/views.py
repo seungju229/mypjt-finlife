70d27fdd688aa08104f5bb9cb0b10ce66774727a
seungju425 <seungju425@gmail.com>
2024-11-01 17:39:01 +0900

    1101 예외 처리 다시 하기

--- finlife/views.py
+++ finlife/views.py
@@ -1,3 +1,119 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.conf import settings
+import requests
+from .models import DepositOptions, DepositProducts
+from .serializers import DepositOptionsSerializer, DepositProductsSerializer
+
+
+
+
 
 # Create your views here.
+@api_view(['GET'])
+def save_deposit_products(request):
+    api_key = settings.API_KEY
+    url = f'http://finlife.fss.or.kr/finlifeapi/depositProductsSearch.json?auth={api_key}&topFinGrpNo=020000&pageNo=1'
+    response = requests.get(url).json()
+
+    for li in response.get('result')['baseList']:
+        fin_prdt_cd= li['fin_prdt_cd']
+        kor_co_nm= li['kor_co_nm']
+        fin_prdt_nm= li['fin_prdt_nm']
+        etc_note= li['etc_note']
+        join_deny = li['join_deny']
+        join_member = li['join_member']
+        join_way = li['join_way']
+        spcl_cnd = li['spcl_cnd']
+    
+        if DepositProducts.objects.filter(
+            fin_prdt_cd=fin_prdt_cd,
+            kor_co_nm=kor_co_nm,
+            fin_prdt_nm=fin_prdt_nm,
+            etc_note=etc_note,
+            join_deny=join_deny,
+            join_member=join_member,
+            join_way=join_way,
+            spcl_cnd=spcl_cnd):
+
+            continue
+    
+        save_data = {
+            'fin_prdt_cd':fin_prdt_cd,
+            'kor_co_nm':kor_co_nm,
+            'fin_prdt_nm':fin_prdt_nm,
+            'etc_note':etc_note,
+            'join_deny':join_deny,
+            'join_member':join_member,
+            'join_way':join_way,
+            'spcl_cnd':spcl_cnd
+        }
+        serializer = DepositProductsSerializer(data = save_data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+
+    for li in response.get('result')['optionList']:
+
+        fin_prdt_cd = li['fin_prdt_cd']
+        intr_rate_type_nm = li['intr_rate_type_nm']
+        intr_rate = li['intr_rate']
+        intr_rate2 = li['intr_rate2']
+        save_trm = li['save_trm']
+        product = DepositProducts.objects.get(fin_prdt_cd=fin_prdt_cd)
+        print(fin_prdt_cd, intr_rate_type_nm, intr_rate, intr_rate2, save_trm, product)
+
+        if save_trm == None:
+            save_trm = -1
+
+        for value in (intr_rate, intr_rate2):
+            if value == None:
+                value = -1.0
+        for value in (fin_prdt_cd, intr_rate_type_nm):
+            if value == None:
+                value = '-1'
+
+
+
+        if DepositOptions.objects.filter(
+            fin_prdt_cd=fin_prdt_cd,
+            intr_rate_type_nm=intr_rate_type_nm,
+            intr_rate=intr_rate,
+            intr_rate2=intr_rate2,
+            save_trm=save_trm,
+            product=product
+        ):
+            continue
+
+        save_data = {
+            'fin_prdt_cd':fin_prdt_cd,
+            'intr_rate_type_nm':intr_rate_type_nm,
+            'intr_rate':intr_rate,
+            'intr_rate2':intr_rate2,
+            'save_trm':save_trm,
+        }
+        serializer = DepositOptionsSerializer(data=save_data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save(product=product)
+        
+    return Response(response)
+
+
+
+
+
+
+
+
+@api_view(['GET', 'POST'])
+def deposit_products(request):
+    pass
+
+@api_view(['GET'])
+def deposit_products_options(request):
+    pass
+
+@api_view(['GET'])
+def top_rate(request):
+    pass
+
